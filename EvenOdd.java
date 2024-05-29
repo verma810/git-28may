69cd9df4aebe16fa03c153d3dec12a573a98aded
verma810 <rohit.verma18@wipro.com>
2024-05-29 16:04:42 +0530

    Update EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,5 +1,5 @@
 public class EvenOdd {
-public static void main(String[] args)
+    public static void main(String[] args)
     {
         // Declaring and initializing integer variable
         int num = 10;
@@ -18,17 +18,5 @@ public static void main(String[] args)
             // odd
             System.out.println("Entered Number is Odd");
         }
-    }
-}
-
-        Scanner reader = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int num = reader.nextInt();
-
-        if(num % 2 == 0)
-            System.out.println(num + " is even");
-        else
-            System.out.println(num + " is odd");
     }
 }
